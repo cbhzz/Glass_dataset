f114dc7b9d3fa25408af0395bdb771346e7a2d48
cbhzz <82866513+cbhzz@users.noreply.github.com>
2024-08-10 08:41:44 +0800

    Delete script directory

--- script/rename.py
+++
@@ -1,9 +0,0 @@
-import os
-path1 = r"E:\Tibet Plateau\dataset1\Glass_Albedo_1km"
-path = r"E:\a"
-
-band_name = ["BSA_VIS", 'WSA_VIS', 'QC_VIS', 'BSA_NIR', 'WSA_NIR', 'QC_NIR', 'BSA_shortwave', 'WSA_shortwave', 'QC_shortwave']
-for name in os.listdir(path):
-    out_file = (path1 + os.sep + str(name.split('.')[-3][1:5]) + os.sep + band_name[1] + os.sep + name.replace('.hdf','') + '.tif')
-    print(out_file)
-
